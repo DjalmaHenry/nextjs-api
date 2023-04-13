4b9c53725e36ec46919f1561548aa558b83aba2a
Djalma Henrique <djalmahenry@outlook.com>
2023-04-13 08:06:43 -0300

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -72,7 +72,7 @@ export default function Home() {
                 Djalma Henrique Silva Lima
               </h1>
               <p className={styles.descriptionPresentation}>
-                <code className={styles.code}>Fullstack Engineer</code>
+                <code className={styles.code}>Full Stack Software Engineer</code>
               </p>
             </div>
           </div>
